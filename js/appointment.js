04bcb27e60d18fbe3697139727d463f047a1adeb
NikitaFrolov-123 <frolov.nekit.07.fr@gmail.com>
2025-06-25 21:35:15 +0300

    исправлены форматы фото, адаптив и js код слайдера

--- js/appointment.js
+++ js/appointment.js
@@ -1,7 +1,6 @@
 document.getElementById('appointment__choice').addEventListener('submit', function(e) {
   const dateInput = document.getElementById('date');
   const selectedDate = new Date(dateInput.value);
-  // Sunday is 0
   if (selectedDate.getDay() === 0) {
     e.preventDefault();
     alert('Запись на воскресенье невозможна. Пожалуйста, выберите другой день.');
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+// В файле index.js
 document.addEventListener('DOMContentLoaded', function() {
-  const swiper = new Swiper('.services__swiper', {
+  const swiper = new Swiper('.services__swiper', {  // Исправлен селектор
     loop: true,
     autoplay: {
       delay: 2000,
@@ -26,16 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
-});
 
-document.querySelectorAll('.header__link').forEach(button => {
-  button.addEventListener('click', () => {
-    const targetId = button.getAttribute('data-target');
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
+  document.querySelectorAll('.header__link').forEach(button => {
+    button.addEventListener('click', () => {
+      const targetId = button.getAttribute('data-target');
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
   });
 });
-
-
